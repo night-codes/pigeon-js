f2adc8711584f492a58b8f52ff4269e787d5285a
Alex Suyun <alexsuyunov@yahoo.com>
2015-11-02 17:47:54 -0500

    Reformatted 2 spaces

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ var beautifyHTML = require('js-beautify').html;
 
 module.exports = function(obj, callback, options) {
 
-    var callback = callback || function() {};
-    var options = options || {};
+  var callback = callback || function() {};
+  var options = options || {};
 
-    if (obj !== null && typeof obj !== 'object') {
-        var error = new TypeError('Expected an object.');
-        return callback(error, null);
-    }
+  if (obj !== null && typeof obj !== 'object') {
+    var error = new TypeError('Expected an object.');
+    return callback(error, null);
+  }
 
-    var html = pigeon.build(obj);
+  var html = pigeon.build(obj);
 
-    if (!options.minify) {
-        html = beautifyHTML(html, {
-            indent_size: options.indentSize || 4
-        });
-    }
+  if (!options.minify) {
+    html = beautifyHTML(html, {
+      indent_size: options.indentSize || 4
+    });
+  }
 
-    callback(null, html);
-    return html;
+  callback(null, html);
+  return html;
 
 };
